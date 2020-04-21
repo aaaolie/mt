3b943b50f2858d6f6fd47d77b76cbf502e897137
敖烈 <3104527996@qq.com>
2020-04-21 17:07:17 +0800

    cli-2.x

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  ENV_CONFIG: '"development"',
+  NODE_ENV: '"dev"',
+  ENV_CONFIG: '"dev"',
   API_ROOT: '"http://(本地环境请求地址)"'
 });
